5e56f37617aeeb3d79864dc735655ca1f393e6b6
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-06-27 21:56:35 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>PieMC</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="color-scheme" content="light only" />
+    <meta name="description" content="PieMC" />
+    <meta property="og:site_name" content="argdjthd" />
+    <meta property="og:title" content="argdjthd" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="PieMC" />
+    <meta property="og:image" content="assets/images/image04" />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="800" />
+    <meta property="og:url" content="https://piemc.org" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <link rel="canonical" href="https://piemc.org" />
+    <link
+        href="https://fonts.googleapis.com/css2?display=swap&family=Archivo:ital,wght@0,100;0,400;1,100;1,400&family=Inter:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700"
+        rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <noscript>
+        <link rel="stylesheet" type="text/css" href="css/noscript.css">
+    </noscript>
+</head>
+<body class="is-loading">
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 40 40"
+        display="none" width="0" height="0">
+        <symbol id="icon-80ea30b5b2fef8b399f9b3c4a14e6bee" viewBox="0 0 40 40">
+            <path
+                d="M35.7,36.6H4.3C3.6,36.6,3,36,3,35.3V34c0-0.7,0.6-1.3,1.3-1.3h31.5c0.7,0,1.3,0.6,1.3,1.3v1.3C37,36.1,36.4,36.6,35.7,36.6 z M21.4,28.3c-0.4,0.4-0.9,0.6-1.5,0.6s-1.1-0.2-1.5-0.6L8,18c-0.4-0.4-0.6-0.9-0.6-1.5S7.6,15.4,8,15l1.2-1.2 c0.4-0.4,0.9-0.6,1.5-0.6s1.1,0.2,1.5,0.6l4.7,4.7V7.3c0-0.6,0.2-1,0.6-1.4c0.4-0.3,0.8-0.5,1.4-0.5h2c0.6,0,1.1,0.2,1.5,0.5 C22.8,6.3,23,6.8,23,7.3v11.2l4.7-4.7c0.4-0.4,0.9-0.6,1.5-0.6s1.1,0.2,1.5,0.6l1.2,1.2c0.4,0.4,0.6,0.9,0.6,1.5s-0.2,1.1-0.6,1.5 L21.4,28.3z" />
+        </symbol>
+        <symbol id="icon-be848c9691a8e3f7630e3908e426bf9f" viewBox="0 0 40 40">
+            <path
+                d="M37.9,10.6l-3.2-3.2c-0.4-0.4-1-0.7-1.6-0.7c-0.6,0-1.2,0.2-1.6,0.7L15.9,23l-7-7c-0.4-0.4-1-0.7-1.6-0.7S6.1,15.6,5.6,16 l-3.2,3.2c-0.4,0.4-0.7,1-0.7,1.6S2,22,2.4,22.5l8.6,8.6l3.2,3.2c0.4,0.4,1,0.7,1.6,0.7c0.6,0,1.2-0.2,1.6-0.7l3.2-3.2l17.2-17.2 c0.4-0.4,0.7-1,0.7-1.6C38.6,11.6,38.4,11.1,37.9,10.6z" />
+        </symbol>
+        <symbol id="icon-67ad41ecc66a1b1cd00d0d3b20c00017" viewBox="0 0 40 40">
+            <path
+                d="M34.1,19.9c0,0.3-0.1,0.3-0.1,0.4L21.3,33.1c-0.1,0.1-0.1,0.1-0.4,0.1c-0.3,0-0.3-0.1-0.4-0.1L19,31.6 c-0.1-0.1-0.1-0.1-0.1-0.4s0.1-0.3,0.1-0.4l9.2-9.2H5.8c-0.1,0-0.2,0-0.2,0c0,0,0,0,0,0c-0.1-0.1-0.1-0.2-0.1-0.4v-2.5 c0-0.2,0.1-0.4,0.1-0.5c0,0,0.1,0,0.2,0h22.4L19,9c-0.1-0.1-0.1-0.1-0.1-0.4c0-0.3,0.1-0.3,0.1-0.4l1.5-1.5c0.1-0.1,0.1-0.1,0.4-0.1 c0.3,0,0.3,0.1,0.4,0.1L34,19.5C34,19.6,34.1,19.6,34.1,19.9z" />
+        </symbol>
+        <symbol id="icon-3b7eeeccbb457780f277fce4669f67a0" viewBox="0 0 40 40">
+            <path
+                d="M33.2,8.3c-2.5-1.1-5.1-1.9-7.9-2.4c-0.3,0.6-0.7,1.4-1,2c-2.9-0.4-5.8-0.4-8.7,0c-0.3-0.6-0.7-1.4-1-2 c-2.8,0.5-5.4,1.3-7.9,2.4c-5,7.2-6.3,14.2-5.6,21.1c3.3,2.3,6.5,3.8,9.6,4.7c0.8-1,1.5-2.1,2.1-3.3c-1.1-0.4-2.2-0.9-3.2-1.5 c0.3-0.2,0.5-0.4,0.8-0.6c6.3,2.8,13,2.8,19.2,0c0.3,0.2,0.5,0.4,0.8,0.6c-1,0.6-2.1,1.1-3.2,1.5c0.6,1.1,1.3,2.2,2.1,3.3 c3.1-0.9,6.3-2.4,9.6-4.7C39.7,21.4,37.5,14.4,33.2,8.3z M13.7,25.1c-1.9,0-3.4-1.7-3.4-3.7s1.5-3.7,3.4-3.7c1.9,0,3.5,1.7,3.4,3.7 C17.1,23.4,15.6,25.1,13.7,25.1z M26.3,25.1c-1.9,0-3.4-1.7-3.4-3.7s1.5-3.7,3.4-3.7c1.9,0,3.5,1.7,3.4,3.7 C29.7,23.4,28.2,25.1,26.3,25.1z" />
+        </symbol>
+        <symbol id="icon-c3c8e1063e3b7f84f6b54712741de139" viewBox="0 0 40 40">
+            <path
+                d="M37.3,15.3v15.3c0,0.8-0.3,1.6-0.9,2.2c-0.6,0.6-1.4,0.9-2.2,0.9H5.8c-0.8,0-1.6-0.3-2.2-0.9s-0.9-1.4-0.9-2.2V15.3 c0.5,0.6,1.2,1.1,2,1.7c4.7,3.1,7.9,5.4,9.6,6.7c0.7,0.5,1.4,0.9,1.8,1.2c0.4,0.3,1,0.6,1.9,0.9c0.7,0.3,1.5,0.5,2.1,0.5l0,0 c0.6,0,1.4-0.2,2.1-0.5c0.7-0.3,1.4-0.6,1.9-0.9c0.4-0.3,1-0.7,1.8-1.2c2.2-1.6,5.4-3.8,9.6-6.7C36.1,16.5,36.7,15.9,37.3,15.3 L37.3,15.3z M37.3,9.6c0,1-0.3,2-0.9,2.9c-0.6,0.9-1.5,1.8-2.4,2.4c-4.9,3.3-7.9,5.4-9,6.2c-0.1,0.1-0.4,0.3-0.8,0.6 c-0.4,0.3-0.7,0.5-1,0.7c-0.3,0.2-0.6,0.4-1,0.6c-0.4,0.2-0.7,0.4-1.1,0.5c-0.4,0.1-0.6,0.2-0.9,0.2l0,0c-0.3,0-0.6-0.1-0.9-0.2 c-0.3-0.1-0.7-0.3-1.1-0.5c-0.4-0.2-0.7-0.4-1-0.6c-0.3-0.2-0.6-0.4-1-0.7c-0.4-0.3-0.7-0.5-0.8-0.6c-1.1-0.8-2.8-2-5.1-3.5 c-2.3-1.6-3.3-2.4-3.7-2.7c-0.8-0.5-1.6-1.2-2.3-2.2s-1-1.9-1-2.6c0-1,0.3-1.9,0.8-2.5c0.5-0.6,1.2-1,2.3-1h28.4 c0.8,0,1.6,0.3,2.2,0.9C37.1,8.1,37.3,8.8,37.3,9.6L37.3,9.6z" />
+        </symbol>
+        <symbol id="icon-8c4b37645de3c276d895d87df51ba614" viewBox="0 0 40 40">
+            <path
+                d="M36,20.3c0,3.5-1,6.6-3.1,9.4c-2,2.8-4.7,4.7-7.9,5.8c-0.4,0.1-0.6,0-0.8-0.1c-0.2-0.2-0.3-0.4-0.3-0.6v-4.4 c0-1.3-0.4-2.3-1.1-3c0.8-0.1,1.5-0.2,2.1-0.4c0.6-0.2,1.3-0.4,2-0.8c0.7-0.4,1.2-0.8,1.7-1.4c0.5-0.5,0.8-1.3,1.1-2.2 s0.4-2,0.4-3.1c0-1.7-0.5-3.1-1.6-4.3c0.5-1.3,0.5-2.7-0.2-4.2c-0.4-0.1-1,0-1.7,0.2c-0.7,0.3-1.4,0.6-1.9,0.9L24,12.5 c-1.3-0.4-2.6-0.5-4-0.5s-2.7,0.2-4,0.5c-0.2-0.2-0.5-0.3-0.9-0.6c-0.4-0.2-0.9-0.5-1.7-0.8c-0.8-0.3-1.4-0.4-1.8-0.3 c-0.6,1.6-0.7,3-0.1,4.2c-1.1,1.2-1.6,2.6-1.6,4.3c0,1.2,0.1,2.2,0.4,3.1s0.6,1.6,1.1,2.2s1,1,1.7,1.4c0.7,0.4,1.3,0.6,2,0.8 c0.6,0.2,1.3,0.3,2.1,0.4c-0.6,0.5-0.9,1.2-1,2.1c-0.3,0.1-0.6,0.2-0.9,0.3c-0.3,0.1-0.7,0.1-1.2,0.1c-0.5,0-0.9-0.1-1.4-0.4 c-0.5-0.3-0.8-0.7-1.2-1.3c-0.3-0.4-0.6-0.8-1-1.1c-0.4-0.3-0.8-0.4-1-0.5L9,26.5c-0.3,0-0.5,0-0.6,0.1c-0.1,0.1-0.1,0.1-0.1,0.2 c0,0.1,0.1,0.2,0.2,0.3c0.1,0.1,0.2,0.2,0.3,0.2l0.1,0.1c0.3,0.1,0.6,0.4,0.9,0.8s0.5,0.7,0.7,1.1l0.2,0.5c0.2,0.5,0.5,1,0.9,1.3 c0.4,0.3,0.9,0.5,1.4,0.6c0.5,0.1,1,0.1,1.4,0.1c0.5,0,0.9,0,1.2-0.1l0.5-0.1c0,0.5,0,1.1,0,1.9c0,0.7,0,1.1,0,1.1 c0,0.2-0.1,0.5-0.3,0.6c-0.2,0.2-0.5,0.2-0.8,0.1c-3.2-1.1-5.8-3-7.9-5.8S4,23.8,4,20.3c0-2.9,0.7-5.6,2.1-8S9.5,7.8,12,6.4 s5.1-2.1,8-2.1s5.6,0.7,8,2.1s4.4,3.4,5.8,5.8S36,17.4,36,20.3L36,20.3z" />
+        </symbol>
+    </svg>
+    <div id="wrapper">
+        <div id="main">
+            <div class="inner">
+                <div id="container05" class="style1 container columns">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1" data-reorder="1,0">
+                            <div>
+                                <h1 id="text05" class="style3">
+                                    <span class="p">
+                                        <strong>
+                                            Modern software.<br />Built to perform.
+                                        </strong>
+                                    </span>
+                                </h1>
+                                <p id="text10" class="style4">PieMC improves Minecraft’s ecosystem with fast, secure
+                                    software and an expanding plugin API, providing quick releases and helpful support
+                                    as the most widely used, performant, and stable software available.</p>
+                                <ul id="buttons01" class="style2 buttons">
+                                    <li>
+                                        <a href="https://github.com/PieMC-Dev/PieMC" class="button n01">
+                                            <svg>
+                                                <use xlink:href="#icon-80ea30b5b2fef8b399f9b3c4a14e6bee"></use>
+                                            </svg>
+                                            <span class="label">Downloads</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/docs" class="button n02">
+                                            <svg>
+                                                <use xlink:href="#icon-be848c9691a8e3f7630e3908e426bf9f"></use>
+                                            </svg>
+                                            <span class="label">Documentation</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <div id="image02" class="style1 image" data-position="left">
+                                    <span class="frame">
+                                        <img src="assets/images/image02.png" alt="" />
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="container03" class="style2 container columns">
+                    <div class="wrapper">
+                        <div class="inner" data-onvisible-trigger="1" data-reorder="1,0">
+                            <div>
+                                <h2 id="text07" class="style5">Fast &amp; Performance</h2>
+                                <p id="text09" class="style4">
+                                    <span class="p">
+                                        The promise of stability<br />Our software is designed with security and
+                                        stability in mind. We have consistently been among the first to fix
+                                        vulnerabilities in the Minecraft space for years, so you can rest assured that
+                                        PieMC software is a good choice.
+                                    </span>
+                                </p>
+                                <ul id="buttons02" class="style1 buttons">
+                                    <li>
+                                        <a href="https://github.com/PieMC-Dev/PieMC" class="button n01">
+                                            <svg>
+                                                <use xlink:href="#icon-67ad41ecc66a1b1cd00d0d3b20c00017"></use>
+                                            </svg>
+                                            <span class="label">PieMC Source</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <div id="image04" class="style2 image" data-position="left">
+                                    <span class="frame">
+                                        <img src="assets/images/image04.png?v=16cd400a" alt="" />
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h2 id="text19" class="style5">PieMC</h2>
+                <p id="text43" class="style1">© PieMC. All rights reserved.</p>
+                <ul id="icons01" class="style1 icons">
+                    <li>
+                        <a class="n01" href="https://discord.gg/6ZJQUJrztp" aria-label="Discord">
+                            <svg>
+                                <use xlink:href="#icon-3b7eeeccbb457780f277fce4669f67a0"></use>
+                            </svg>
+                            <span class="label">Discord</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="n02" href="mailto:todo" aria-label="Email">
+                            <svg>
+                                <use xlink:href="#icon-c3c8e1063e3b7f84f6b54712741de139"></use>
+                            </svg>
+                            <span class="label">Email</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="n03" href="https://github.com/PieMC-Dev/PieMC" aria-label="GitHub">
+                            <svg>
+                                <use xlink:href="#icon-8c4b37645de3c276d895d87df51ba614"></use>
+                            </svg>
+                            <span class="label">GitHub</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+</html>
